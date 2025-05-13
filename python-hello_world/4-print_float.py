24d0adfe355aeabbe942847f7d190dfd634f695e
z502wa <z502wa@gmail.com>
2025-05-13 18:18:20 +0000

    Python - Hello, World

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 # 4-print_float.py: prints a float with 2 decimal places
-
 number = 3.14159  # define the float variable
-print(f"Float: {number:.2f}")  
+print(f"Float: {number:.2f}")  # display the float with precision of 2 digits
