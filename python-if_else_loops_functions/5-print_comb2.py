2e8e59c3cd7fb2933ceecb39548acfa90b42ac90
z502wa <z502wa@gmail.com>
2025-05-13 20:42:27 +0000

    holberton

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 # 5-print_comb2.py: prints numbers from 00 to 99 separated by ", "
 for i in range(100):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+    if i < 99:
+        print(f"{i:02d}", end=", ")
+    else:
+        print(f"{i:02d}")
