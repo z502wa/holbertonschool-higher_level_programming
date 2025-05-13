2a1ceacb1f5b20fbf11c04fcac623d5a24ad9ed9
z502wa <z502wa@gmail.com>
2025-05-13 20:57:03 +0000

    holberton

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print("{:s}".format(result))
