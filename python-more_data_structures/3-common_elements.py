db33404e16e9495ac756b864b712bf394804cf89
z502wa <z502wa@gmail.com>
2025-05-16 23:05:18 +0000

    holberton

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    Return a set of common elements in two sets.
+    """
+    # Use set intersection to find common elements
+    return set_1 & set_2
