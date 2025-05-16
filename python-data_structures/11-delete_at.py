40e80ddce87b2b11dc4060dea402e0b232d69c10
z502wa <z502wa@gmail.com>
2025-05-16 22:52:19 +0000

    holberton

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -7,6 +7,6 @@ def delete_at(my_list=[], idx=0):
     """
     if idx < 0 or idx >= len(my_list):
         return my_list
-    # remove the element at the given index without using pop()
+    # delete the item at the given index
     del my_list[idx]
     return my_list
