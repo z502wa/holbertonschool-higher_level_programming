f7dca11be34e30a519a6c3ca290531fdb9fc2050
z502wa <z502wa@gmail.com>
2025-05-18 16:49:39 +0000

    holberton

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python3
 
-def list_division(my_list_1, my_list_2, list_length):
-    """
-    Divides element by element two lists.
-    Prints error messages on exception and appends 0 for failures.
-    Returns a new list of length list_length with all division results.
-    """
-    result = []
-    for i in range(list_length):
-        try:
-            # attempt to divide corresponding elements
-            res = my_list_1[i] / my_list_2[i]
-        except ZeroDivisionError:
-            print("division by 0")
-            res = 0
-        except IndexError:
-            print("out of range")
-            res = 0
-        except (TypeError, ValueError):
-            print("wrong type")
-            res = 0
-        finally:
-            # always append a result (float or 0)
-            result.append(res)
-    return result
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError()
