537ebc560907d14de73fd38c3b70212e37c6b6b3
z502wa <z502wa@gmail.com>
2025-05-18 16:38:56 +0000

    holberton

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-    """
-    Prints the first x elements of my_list on the same line if they are integers.
-    Skips non-integer values silently.
-    Returns the number of integers printed.
-    """
     count = 0
-    idx = 0
-    while idx < x:
+    # iterate over up to x elements of the list
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[idx]), end="")
+            # try to format and print the current element as an integer
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (TypeError, ValueError):
-            pass
-        idx += 1
-    print()
+        except (ValueError, TypeError):
+            # if formatting fails (not an int), skip this element silently
+            continue
+    print()  # finish with a newline
     return count
