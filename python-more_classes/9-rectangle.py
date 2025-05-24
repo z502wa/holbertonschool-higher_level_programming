f11cca4ee9884a9b66f83f322d650b6b0f25a72b
z502wa <z502wa@hmail.com>
2025-05-24 18:12:39 +0000

    holberton

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -122,41 +122,11 @@ class Rectangle:
         """
         Return a string to recreate a new instance via eval().
 
-        Format: Rectangle(width, height)
+        Rectangle(width, height)
         """
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        """
-        Print a message on deletion and update instance count.
-        """
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """
-        Return the rectangle with the greater area.
-
-        Args:
-            rect_1 (Rectangle): first rectangle.
-            rect_2 (Rectangle): second rectangle.
-
-        Raises:
-            TypeError: if rect_1 or rect_2 is not a Rectangle.
-
-        Returns:
-            Rectangle: the rectangle with the larger area or rect_1 if equal.
-        """
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
-
-    @classmethod
     def square(cls, size=0):
         """
         Create and return a new Rectangle instance with width == height == size.
