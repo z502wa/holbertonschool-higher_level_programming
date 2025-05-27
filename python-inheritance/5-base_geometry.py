e0911cc16a3a9783c8c953a362bbdf7c825f2627
z502wa <z502wa@gmail.com>
2025-05-27 15:39:10 +0000

    holberton

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Suhail Alaboud
+10675@holbertonstudents.com
+
+This module contains an empty BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Empty BaseGeometry class
+    
+    This class serves as a base class for geometric shapes
+    """
+    pass
