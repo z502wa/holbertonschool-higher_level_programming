c99993dcd6c92cce1a7d2061929c40164e2ace05
z502wa <z502wa@gmail.com>
2025-06-02 17:55:56 +0000

    Add task 1: update write file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -11,14 +11,14 @@ def write_file(filename="", text=""):
     """
     Writes a string to a text file (UTF-8) and returns number of characters
     written.
-    
+
     Args:
         filename (str): Path to the file to write. Defaults to empty string.
         text (str): Text content to write to file. Defaults to empty string.
-        
+
     Returns:
         int: Number of characters written to the file.
     """
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, "w", encoding="utf-8") as file:
         characters_written = file.write(text)  # Write text and get count
         return characters_written  # Return number of characters written
