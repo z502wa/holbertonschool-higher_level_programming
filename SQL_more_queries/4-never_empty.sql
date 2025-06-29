9bcca9f0583cf1d0490cbcc6b9a1d093769f612b
z502wa <z502wa@gmail.com>
2025-06-29 11:03:52 +0000

    holberton

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
--- Creates the table force_name on your MySQL server
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
+-- Creates the table id_not_null on your MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL,
+    name VARCHAR(256)
 );
