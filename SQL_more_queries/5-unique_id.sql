8aa854fdd383d7797def078c70ee06401b90380a
z502wa <z502wa@gmail.com>
2025-06-29 11:07:01 +0000

    holberton

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
--- Creates the table id_not_null on your MySQL server
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT NOT NULL,
+-- Creates the table unique_id on your MySQL server
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
