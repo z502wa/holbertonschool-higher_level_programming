d546e3fe843790f43146f2027c4ca3ef8ea7191c
z502wa <z502wa@gmail.com>
2025-07-13 16:39:53 +0000

    task7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const n = parseInt(process.argv[2]);
+
+if (Number.isNaN(n)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < n; i++) {
+    console.log('C is fun');
+  }
+}
